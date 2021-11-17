18611a7d6bd710a78c5d22abb4d6fc842409c024
Abin Mn <abinmn@users.noreply.github.com>
2021-11-17 17:16:01 +0530

    Zara, Abin set terraform backend to S3

---
+++ state.tf
@@ -0,0 +1,8 @@
+terraform {
+ backend "s3" {
+   bucket         = "tf-state-bucket-todo-app"
+   key            = "state/terraform.tfstate"
+   region         = "eu-central-1"
+   dynamodb_table = "tf-state-db"
+ }
+}
