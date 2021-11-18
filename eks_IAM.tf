673e24842e6fce2e7b3fbe1c8f24c9a9483d6c7d
Zahra Rezaie <icarus@Zahras-MBP.fritz.box>
2021-11-18 10:31:28 +0100

    Abin, Zara add IAM roles and policies

---
+++ eks_IAM.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_role" "todo_eks_cluster" {
+  name = "todo-eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "todo-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.todo_eks_cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "todo-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.todo_eks_cluster.name
+}
