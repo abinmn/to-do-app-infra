0d5957896cf884b0f6daf4897f3cc08ddf047cc3
Abin Mn <abinmn@users.noreply.github.com>
2021-11-18 11:20:31 +0530

    Abin, Zara update ecr to use immutable image tag

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,4 @@
-resource "aws_ecr_repository" "ecr_tw_infra" {
-  name = "ecr-tw-infra"
+resource "aws_ecr_repository" "ecr_todo_app" {
+  name = "to-do-app"
+  image_tag_mutability = "IMMUTABLE"
 }
